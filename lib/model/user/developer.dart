af56e44fd5a5afcc81de7d12679bb9393845c9cd
Nur Fajar Sayyidul Ayyam <nur.fajar11@ui.ac.id>
2022-07-11 20:15:02 +0700

    develop user model

--- lib/model/user/developer.dart
+++ lib/model/user/developer.dart
@@ -1,14 +1,13 @@
 import 'package:simple_todo/model/user/user.dart';
 
-class Developer extends User {
+class Developer{
   static const String _name = "Nur Fajar Sayyidul Ayyam";
   static const String _role = "Mobile Developer - UI/UX Designer";
   static const String _nickname = "Fajar";
   static const String _hobby = "Code";
   static const String _institution = "University of Indonesia";
 
-  Developer() : super(name: _name);
-
+  get name => _name;
   get role => _role;
   get nickname => _nickname;
   get hobby => _hobby;
--- lib/model/user/user.dart
+++ lib/model/user/user.dart
@@ -1,5 +1,49 @@
+import 'package:simple_todo/common/constant.dart';
+import 'package:simple_todo/model/task/task.dart';
+
 class User {
   String name;
-  User({required this.name});
+  List<Task> tasks = [];
+
   set setName(String name) => this.name = name;
+
+  User({
+    required this.name,
+  });
+
+  int countTodo() {
+    int temp = 0;
+    if (tasks.isNotEmpty) {
+      for (var task in tasks) {
+        if (task.status?.current == kTodo) {
+          temp++;
+        }
+      }
+    }
+    return temp;
+  }
+
+  int countInprogress() {
+    int temp = 0;
+    if (tasks.isNotEmpty) {
+      for (var task in tasks) {
+        if (task.status?.current == kInprogress) {
+          temp++;
+        }
+      }
+    }
+    return temp;
+  }
+
+  int countComplete() {
+    int temp = 0;
+    if (tasks.isNotEmpty) {
+      for (var task in tasks) {
+        if (task.status?.current == kComplete) {
+          temp++;
+        }
+      }
+    }
+    return temp;
+  }
 }
